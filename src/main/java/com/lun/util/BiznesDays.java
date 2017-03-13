5290e04db39af083deb815731fb9861514692238
lubich <evg>
2017-03-13 13:44:05 +0200

    WorkingMonthes displaying

--- src/main/java/com/lun/util/BiznesDays.java
+++ src/main/java/com/lun/util/BiznesDays.java
@@ -1,6 +1,5 @@
 package com.lun.util;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -9,20 +8,11 @@ import java.util.Date;
  */
 public class BiznesDays {
 
-    private int year;
-    private int month;
-    private int day;
     private Date startDate;
     private Date endDate;
     Calendar startCal = Calendar.getInstance();
     Calendar endCal = Calendar.getInstance();
 
-
-//    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-//    String dateInString = "01-07-2016";
-//    Date startDate = sdf.parse(dateInString);
-//    String dateInString2 = "31-07-2016";
-//    Date endDate = sdf.parse(dateInString2);
 
     public BiznesDays(int year, int month, int day){
         this.startCal.set(year, month, day);
@@ -30,24 +20,11 @@ public class BiznesDays {
     }
 
 
-    public int calculateDuration()
-    {
-        //Calendar startCal = Calendar.getInstance();
-        //startCal.setTime(startDate);
-
-        //Calendar endCal = Calendar.getInstance();
-        //endCal.setTime(endDate);
+    public int calculateDuration() {
 
         int workDays = 0;
 
-        if (startCal.getTimeInMillis() > endCal.getTimeInMillis())
-        {
-            startCal.setTime(endDate);
-            endCal.setTime(startDate);
-        }
-
-        do
-        {
+        do {
             startCal.add(Calendar.DAY_OF_MONTH, 1);
             if (startCal.get(Calendar.DAY_OF_WEEK) != Calendar.FRIDAY && startCal.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY)
             {
